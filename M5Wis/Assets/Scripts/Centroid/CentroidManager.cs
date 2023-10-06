dc94ce0091e47eb559bfbff792febfa2f31d24a6
Gino Schaap <35615@ma-web.nl>
2023-10-06 14:44:24 +0200

    hAB

--- M5Wis/Assets/Scripts/Centroid/CentroidManager.cs
+++ M5Wis/Assets/Scripts/Centroid/CentroidManager.cs
@@ -10,18 +10,24 @@ public class CentroidManager : MonoBehaviour
 
     public GameObject AB;
     public GameObject AC;
+    public GameObject BC;
     
     private LineRenderer lrAB;
     private LineRenderer lrAC;
+    private LineRenderer lrBC;
 
 
     private LinearFunction fab = new LinearFunction(1,1);
     private LinearFunction fac = new LinearFunction(1,1);
+    private LinearFunction fbc= new LinearFunction(1,1);
+
+    public GameObject hAB;
     // Start is called before the first frame update
     void Start()
     {
         lrAB = AB.GetComponent<LineRenderer>();
         lrAC = AC.GetComponent<LineRenderer>();
+        lrBC = BC.GetComponent<LineRenderer>();
     }
 
     // Update is called once per frame
@@ -36,5 +42,12 @@ public class CentroidManager : MonoBehaviour
         fac.LinetroughTwoPoint(A.transform.position, C.transform.position);
         lrAC.SetPosition(0, new Vector3(-10, fac.GetY(-10), 0));
         lrAC.SetPosition(1, new Vector3(10, fac.GetY(10), 0));
+
+        fbc.LinetroughTwoPoint(B.transform.position, C.transform.position);
+        lrBC.SetPosition(0, new Vector3(-10, fbc.GetY(-10), 0));
+        lrBC.SetPosition(1, new Vector3(10, fbc.GetY(10), 0));
+
+        hAB.transform.position = new Vector3((A.transform.position.x + B.transform.position.x) /2 , (A.transform.position.y + B.transform.position.y) /2,0);
+
     }
 }
