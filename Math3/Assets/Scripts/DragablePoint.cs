5c3da9aeb8c74973541955d6eccba501e86400ae
Gino Schaap <35615@ma-web.nl>
2024-09-10 14:23:21 +0200

    new start of the Drag Point

---
+++ Math3/Assets/Scripts/DragablePoint.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class DragablePoint : MonoBehaviour
+{
+    private Vector3 mousePosition;
+    public bool isDrag = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 mousePosition = Input.mousePosition;
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+
+        if (isDrag)
+        {
+            this.transform.position = new Vector3(mousePosition.x, mousePosition.y, 0);
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        Debug.Log("eet kaas");
+        isDrag = true;
+    }
+
+    private void OnMouseUp()
+    {
+        isDrag = false;
+        Debug.Log("soup");
+    }
+}
